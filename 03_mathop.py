955b7175742485d164e2c0ff8d452af5c6eb019f
Uma Lakshmi Kalakada <79893832+Uma-Kalakada@users.noreply.github.com>
2021-03-10 12:34:20 +0530

    Add files via upload

---
+++ 03_mathop.py
@@ -0,0 +1,27 @@
+# program to perform all mathematical operations on two numbers.
+
+a = 4
+b = 2
+sum = a + b
+print("The sum of", a, "and", b, "is", sum)
+print(f"The sum of {a} and {b} is {a+b}.")
+
+diff = a - b
+print("The difference between", a, "and", b,"is", diff)
+print(f"The difference between {a} and {b} is {diff}.")
+
+prod = a * b
+print(f"The product of {a} and {b} is {prod}.")
+
+quotient = a/b
+print(f"The result of {a} divided by {b} is {quotient}.")
+
+rem = a%b
+print(f"The remainder of {a} when divided by {b} is {rem}.")
+
+#gives integer 
+floor_quotient = a//b
+print(f"the result of floor division of {a} by {b} is {floor_quotient}.")
+
+exp = a**b
+print(f"The result of {a} raising to the power of {b} is {exp}.")
---
+++ 04_augmn_operators.py
@@ -0,0 +1,33 @@
+# program to perform all augmented operations on two numbers.
+
+a = 4
+b = 2
+
+print(f"a = {a} and b= {b}")
+a += b  
+print(f"The result of a+=b is {a}.")
+
+print(f"a = {a} and b= {b}")
+a -= b 
+print(f"The result of a-=b is {a}.")
+
+print(f"a = {a} and b= {b}")
+a *= b 
+print(f"The result of a*=b is {a}.")
+      
+print(f"a = {a} and b= {b}")
+a /= b 
+print(f"The result of a/=b is {a}.")
+
+      
+print(f"a = {a} and b= {b}")
+a //= b 
+print(f"The result of a//=b is {a}.")
+
+print(f"a = {a} and b= {b}")
+a **= b 
+print(f"The result of a**=b is {a}.")
+
+print(f"a = {a} and b= {b}")
+a %= b 
+print(f"The result of a%=b is {a}.")
